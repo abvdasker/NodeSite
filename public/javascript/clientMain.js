0b949fe4b74404d514526d79cad39fffe06778fb
Hal Rogers <hal.c.rogers@gmail.com>
2013-10-15 21:51:51 -0400

    - added a format for article titles - more styling - general beutification

--- public/javascript/clientMain.js
+++ public/javascript/clientMain.js
@@ -1,17 +1,9 @@
 $(function() {
     // parallax
+    ImageController.adjustBackground();
+
     var $image = $("#background");
-    
-    var docWidth = $(window).width();
-    var widths = [1280, 1920, 4096, 5600];
-    
-    for(var i = 0; i < widths.length; i++) {
-        if (docWidth <= widths[i] || i == widths.length-1) {
-            $image.prop("src", "static/image/Painting-Abstracts"+widths[i]+".jpg")
-            break;
-        }
-    }
-    
+    // initialize parallax scroll
     var bodySpeed = 8.5;
     var $window = $(window);
     $window.scroll(function() {
@@ -19,10 +11,49 @@ $(function() {
         var coords = yPos +'px'  
         $image.css({top: coords});
     });
-    //molt.discover();
+    
+    $(window).resize(ImageController.resizeBackground);
+    
 });
 
 $(window).load(function(){
     // keeps transition from firing on page load
     $("body").removeClass("preload");  
 });
+
+var ImageController = {
+  adjustBackground : function() {
+    var $image = $("#background");
+    
+    var docWidth = $(window).width();
+    var widths = [1060, 1280, 1920, 4096, 5600];
+    
+    for(var i = 0; i < widths.length; i++) {
+        if (docWidth <= widths[i] || i == widths.length-1) {
+            $image.prop("src", "http://localhost:8000/static/image/Painting-Abstracts"+widths[i]+".jpg")
+            break;
+        }
+    }
+  },
+  
+  resizeBackground : function() {
+    var docWidth = $(window).width();
+    var m = ImageController.isMobile();
+    
+    if (docWidth < 1060 && !m) {
+      var $image = $("#background");
+      $image.prop("src", "http://localhost:8000/static/image/Painting-Abstracts"+1060+".jpg");
+    }
+    
+    if (docWidth >= 1060 && m) {
+      var $image = $("#background");
+      $image.prop("src", "http://localhost:8000/static/image/Painting-Abstracts"+1280+".jpg");
+    }
+  },
+  
+  isMobile : function() {
+    var $image = $("#background");
+    var s = $image.prop("src").split("s");
+    return s[s.length-1] == "1060.jpg"
+  }
+}
