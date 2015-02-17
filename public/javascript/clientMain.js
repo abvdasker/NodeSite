d6a5a31f818d2140bfefe40c3ad68e17ef72668d
Hal Rogers <hal.c.rogers@gmail.com>
2015-02-16 19:25:51 -0500

    adds frosted glass styling/structure to the project with parallax compatibility. Classy as fuck.

--- public/javascript/clientMain.js
+++ public/javascript/clientMain.js
@@ -8,9 +8,9 @@ $(function() {
         var coords = yPos +'px'  
         $image.css({top: coords});
     });*/
-    var r = ImageController.resizeBackground;
-    $window.resize(r);
-    r();
+    //var r = ImageController.resizeBackground;
+    $window.resize(ImageController.resizeBackground.bind(ImageController));
+    ImageController.resizeBackground();
     
     $(".con_year").click(function(e){
       $(this).children(".collapse").toggle(200);
@@ -38,7 +38,7 @@ $(function() {
                 window.setTimeout(callback, 1000 / 60);
               };
     })();
-    window.requestAnimFrame(ImageController.parallax);
+    window.requestAnimFrame(ImageController.parallax.bind(ImageController));
 });
 
 $(window).load(function(){
@@ -54,36 +54,60 @@ var ImageController = {
   $background : null,
   imageStr : null,
   dirty : true,
+  bodySpeed: 8.5,
+  lastScrollTop: null,
   
   init : function() {
-    $window = $(window);
-    docWidth = $window.width();
-    widths = [1060, 1280, 1920, 4096, 5600];
-    $background = $("#background");
+    this.$window = $(window);
+    this.docWidth = this.$window.width();
+    this.widths = [1060, 1280, 1920, 4096, 5600];
+    this.$background = $("#background");
   },
   
   parallax : function() {
-        if (this.dirty) {
-          $background.prop("src", this.imageStr); 
-          this.dirty = false;
-        }
-        var bodySpeed = 8.5;
-        var yPos = -($window.scrollTop() / bodySpeed);
-        var coords = yPos;  
-        $background.css({top: coords});
-        window.requestAnimFrame(ImageController.parallax);
+    if (this.dirty) {
+      this.$background.prop("src", this.imageStr); 
+      this.dirty = false;
+    }
+    var shouldUpdate = false;
+    var currentScrollTop = this.$window.scrollTop();
+    if (this.lastScrollTop == null) {
+      shouldUpdate = true;
+    } else if (this.lastScrollTop != currentScrollTop) {
+      shouldUpdate = true;
+    }
+
+    if (shouldUpdate) {
+      var bodySpeed = this.bodySpeed
+      var yPos = -(currentScrollTop / bodySpeed);
+      this.$background.css({top: yPos});
+
+      var articleBackgrounds = $(".glass");
+      articleBackgrounds.each(function(idx, elem) {
+        var $elem = $(elem);
+        // var topOffset = $elem.offset().top;
+        // var totalOffset = topOffset + currentScrollTop;
+        // var yPos = -(totalOffset / bodySpeed);
+        // var newPosition = "42% " + yPos + "px";
+        $elem.css({"background-position": "42% " + yPos + "px"});
+      });
+    }
+
+    
+    this.lastScrollTop = currentScrollTop;
+    window.requestAnimFrame(ImageController.parallax.bind(ImageController));
   },
   
   resizeBackground : function() {
-    var docWidth = $window.width();
+    var docWidth = this.$window.width();
     var s;
     if (docWidth < 1124) {         
       s = "/static/image/Painting-Abstracts"+1024+".jpg";
     } else if (docWidth < 1380) {
       s = "/static/image/Painting-Abstracts"+1280+".jpg";
-    }else if (docWidth < 2020) {          
+    } else if (docWidth < 2020) {          
       s = "/static/image/Painting-Abstracts"+1920+".jpg";
-    } else if (docWidth < 4196) {          
+    } else {          
       s = "/static/image/Painting-Abstracts"+4096+".jpg";
     }
     
@@ -95,7 +119,7 @@ var ImageController = {
   },
   
   isMobile : function() {
-    var $image = $background;
+    var $image = this.$background;
     var s = $image.prop("src").split("s");
     return this.imageWidth() < 1080;
   },
